c3464510d5ae0a06f200edad52f829d788ab2322
Tim Dacalos <2004tim2004@gmail.com>
2025-06-24 14:47:35 -0700

    light/dark mode added (no button)

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -50,12 +52,18 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
         GeistSans.variable,
         GeistMono.variable
       )}
+      suppressHydrationWarning
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto" suppressHydrationWarning>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
@@ -63,6 +71,7 @@ export default function RootLayout({
           <Analytics />
           <SpeedInsights />
         </main>
+        </ThemeProvider>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { BlogPosts } from 'app/components/posts'
 import Image from 'next/image'
+import { Badge } from "@/components/ui/badge"
+
 
 export default function Page() {
   return (
@@ -11,16 +13,20 @@ export default function Page() {
         height={125}
         className='mb-1'
       />
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-5 text-2xl font-semibold tracking-tighter">
         Tim Dacalos
       </h1>
     <p className="mb-4">
       {`Hey everyone, my name is Tim. I'm a fourth-year Computer Science student at the University of California: Irvine, specializing in Systems and Software. I'm passionate about understanding how technology works at its core—from the hardware/software interface to building robust, high-performance systems.`}
     </p>
     <p className="mb-4">
-  {`I initially gravitated towards coding to help others, and my journey has led me to explore low-level programming, operating systems, and parallel computing. I'm actively seeking summer internships to apply and expand these skills.`}
+      {`I initially gravitated towards coding to help others, and my journey has led me to explore low-level programming and operating systems. I'm actively seeking summer internships to apply and expand these skills.`}
     </p>
-    <p>{`Outside of the computer, you'll find me indulging in J-RPGs and discussing the newest anime.`}</p>
+    <p className='mb-4'>{`Outside of the computer, you'll find me indulging in J-RPGs and discussing the newest anime.`}</p>
+    <h2 className="mb-2 text-xl font-semibold tracking-tighter">
+        Skills
+    </h2>
+    <Badge variant="secondary">React</Badge>
       <div className="my-8">
         <BlogPosts />
       </div>
