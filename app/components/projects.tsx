f616b7a2024ef146cc7e143ab157f0702a4752fb
Tim Dacalos <2004tim2004@gmail.com>
2025-06-26 05:53:25 -0700

    couple more projects

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -11,7 +11,8 @@ import Image  from 'next/image'
 import { Badge } from "app/components/ui/badge"
 export function Projects(){
     return(
-        <Card>
+        <>
+        <Card className="mb-4">
             <CardHeader>
                 <CardTitle>Seiyuu Hub</CardTitle>
                 <CardDescription>
@@ -24,7 +25,7 @@ export function Projects(){
                     and discover content about their favorite seiyuu (japanese voice actor).
                     Users can create posts, view posts, and interact with them by commenting or
                     upvoting.
-                    </CardDescription>
+                </CardDescription>
                     <CardAction><a href="https://seiyuuhub.netlify.app/" target="_blank">↗</a></CardAction>
             </CardHeader>
             <CardContent>
@@ -39,5 +40,60 @@ export function Projects(){
                 </div>
             </CardContent>
         </Card>
+        <Card className="mb-4">
+            <CardHeader>
+                <CardTitle>Fabflix Movie Database</CardTitle>
+                <CardDescription>
+                    <div className="flex flex-wrap gap-2 mb-2">
+                    <Badge variant={'secondary'}>JavaScript</Badge>
+                    <Badge variant={'secondary'}>HTML/CSS</Badge>
+                    <Badge variant={'secondary'}>MySQL</Badge>
+                    <Badge variant={'secondary'}>AWS (EC2)</Badge>
+                    <Badge variant={'secondary'}>Docker</Badge>
+                    <Badge variant={'secondary'}>Apache Tomcat</Badge>
+                    </div>
+                    Fabflix is a full stack application built from scratch and powered by AWS.
+                    It utilizes over 50K rows of movie data and is compromised of over 20 features,
+                    including a secure login system, full-text search, cart checkout, and reCAPTCHA.
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="relative aspect-video w-full mb-1">
+                    <Image 
+                        src="/Fabflix.png"
+                        alt="Fabflix Project"
+                        fill
+                        quality={100}
+                        className="object-cover" 
+                    />
+                </div>
+            </CardContent>
+        </Card>
+        <Card>
+            <CardHeader>
+                <CardTitle>Minesweeper AI</CardTitle>
+                <CardDescription>
+                    <div className="flex flex-wrap gap-2 mb-2">
+                    <Badge variant={'secondary'}>Python</Badge>
+                    <Badge variant={'secondary'}>AI</Badge>
+                    </div>
+                    With a team of 3, our group competed in a class wide tournament where we created
+                    our own AI to beat Minesweeper. Our AI managed to beat boards with a 66% win rate.
+                    Our AI used advanced-constraint propagation and increased scores by 37% for 16x16 boards.
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="relative aspect-video w-full mb-1">
+                    <Image 
+                        src="/MinesweeperAI.png"
+                        alt="Minesweeper Project"
+                        fill
+                        quality={100}
+                        className="object-cover" 
+                    />
+                </div>
+            </CardContent>
+        </Card>
+        </>
     )
 }
