30c991292bfc9c042a657bd37953ab3a88213cd0
Tim Dacalos <2004tim2004@gmail.com>
2026-02-17 11:28:43 -0800

    removed that ugly noise

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
-import { NoiseBackground } from './components/ui/background'
+
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
@@ -58,7 +58,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto" suppressHydrationWarning>
-        <NoiseBackground/>
+
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
